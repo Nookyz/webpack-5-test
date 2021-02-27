3dc6373ec87cd20c8b7301da61c40d35acd237e7
Nookyz <djlive@ukr.net>
2021-02-27 19:08:29 +0200

    store

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,10 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { createGlobalStyle } from 'styled-components'
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 import { normalize } from 'styled-normalize'
 
-// import store from '@/store';
+import store from '@/store'
 
 const Home = lazy(() => import('@components/Home'))
 const About = lazy(() => import('@components/About'))
@@ -29,20 +29,20 @@ const GlobalStyle = createGlobalStyle`
 
 const App: React.FC = () => (
   <BrowserRouter>
-    {/* <Provider store={store}> */}
-    <GlobalStyle />
-    <Suspense fallback={<div>loading...</div>}>
-      <Switch>
-        <Route path='/about' exact>
-          <About />
-        </Route>
-        <Route path='/' exact>
-          <Home />
-        </Route>
-      </Switch>
-      <Navigation />
-    </Suspense>
-    {/* </Provider> */}
+    <Provider store={store}>
+      <GlobalStyle />
+      <Suspense fallback={<div>loading...</div>}>
+        <Switch>
+          <Route path='/about' exact>
+            <About />
+          </Route>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+        </Switch>
+        <Navigation />
+      </Suspense>
+    </Provider>
   </BrowserRouter>
 )
 
